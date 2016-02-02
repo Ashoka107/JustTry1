96e200e90dd67550d984b10b05bb450b7babeb53
Ashok M Khote <AshokK@bitwiseglobal.net>
2016-02-02 14:41:54 +0530

    Initial commit

---
+++ src/main/java/Sum.java
@@ -0,0 +1,16 @@
+
+public class Sum {
+    int payment=20000;
+    int reward=10000;
+
+    public void sum()
+    {
+        System.out.println(payment+" + "+reward+" = "+(payment+reward));
+    }
+
+    public static void main(String args[])
+    {
+        Sum s1=new Sum();
+        s1.sum();
+    }
+}
